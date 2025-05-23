fbf473ecc8604327fafcd22af66b05e12b8fbbd4
phillvdm <phillvdm@gmail.com>
2025-05-23 16:11:20 -0400

    chore: update game files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
-  publicDir: 'assets',
+  publicDir: 'public',
   build: {
     rollupOptions: {
       input: 'index.html',
